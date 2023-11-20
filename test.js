71ba9eb2599a29a57b79c017cf74ce935d272629
vrrad <113424121+vrrad@users.noreply.github.com>
2023-11-20 11:02:39 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,5 @@
+const scripty = document.createElement("script");
+const alerty = document.createTextNode("alert()");
+scripty.appendChild(alerty);
+const element = document.getElementById("level6");
+element.appendChild(scripty);
