8c3e8b6f7699950763ecc4e511f36d62562f6f68
vrrad <113424121+vrrad@users.noreply.github.com>
2023-11-28 12:10:18 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,1 @@
-const scripty = document.createElement("script");
-const alerty = document.createTextNode("alert()");
-scripty.appendChild(alerty);
-const element = document.getElementById("log");
-element.appendChild(scripty);
+alert();
